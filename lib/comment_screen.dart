76b16ff7fdcc0496d200fe1d300d278fb3c42aaa
RobinSinghChouhan <robinsingh.02@gmail.com>
2022-04-05 15:08:26 +0530

    Comment Screen Designed.

--- lib/comment_screen.dart
+++ lib/comment_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'classifier.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CommentScreen extends StatefulWidget {
   const CommentScreen({Key? key, required this.postId, this.user})
@@ -18,6 +20,8 @@ class _CommentScreenState extends State<CommentScreen> {
   late Classifier _classifier;
   List<Comments> commentsList = [];
   int status = 0;
+  double _positive = 0.0;
+  double _negative = 0.0;
 
   @override
   void initState() {
@@ -86,49 +90,208 @@ class _CommentScreenState extends State<CommentScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomSheet: Container(
-        margin: EdgeInsets.only(bottom: 10.0, left: 10.0, right: 10.0),
+        color: Colors.white,
+        padding: const EdgeInsets.only(bottom: 10.0, left: 15.0, right: 15.0),
         width: MediaQuery.of(context).size.width,
         child: Row(
           children: [
             Expanded(
               child: TextField(
+                cursorColor: Color(0xff1f0a45),
+                decoration: const InputDecoration.collapsed(
+                    hintText: "Type Comment Here"),
+                style: GoogleFonts.notoSans(
+                  fontSize: 18.0,
+                ),
                 controller: _controller,
               ),
               // width: MediaQuery.of(context).size.width - 80,
             ),
             TextButton(
-                onPressed: () {
-                  if (_controller.text != "") {
-                    final prediction = _classifier.classify(_controller.text);
-                    if (prediction[1] < prediction[0]) {
-                      status = 0;
-                    } else {
-                      status = 1;
-                    }
-                    addComment(status);
-                    print("POSITIVE: " + status.toString());
+              onPressed: () {
+                if (_controller.text != "") {
+                  final prediction = _classifier.classify(_controller.text);
+                  _positive = prediction[1];
+                  _negative = prediction[0];
+                  if (prediction[1] < prediction[0]) {
+                    status = 0;
+                  } else {
+                    status = 1;
                   }
-                },
-                child: Text("Send"))
+                  addComment(status);
+                  print("POSITIVE: " + _positive.toString());
+                }
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Container(
+                          height: MediaQuery.of(context).size.height * 0.25,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Detected Comment",
+                                style: GoogleFonts.notoSans(
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 20.0,
+                              ),
+                              LinearProgressIndicator(
+                                value: _positive,
+                                backgroundColor: const Color(0xffE5F4F0),
+                                valueColor: const AlwaysStoppedAnimation<Color>(
+                                    Color(0xff52DCB3)),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text("Positive(%): " +
+                                  (_positive * 100).toString().substring(0, 5)),
+                              const SizedBox(
+                                height: 20.0,
+                              ),
+                              LinearProgressIndicator(
+                                value: _negative,
+                                backgroundColor: const Color(0xffF5EBEB),
+                                valueColor: const AlwaysStoppedAnimation<Color>(
+                                    Color(0xffE86666)),
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              Text("Negative(%): " +
+                                  (_negative * 100).toString().substring(0, 5)),
+                              const SizedBox(
+                                height: 30.0,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  addComment(status);
+                                  print("POSITIVE: " + _positive.toString());
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0xff1f0a45),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 50.0, vertical: 10.0),
+                                  child: Text(
+                                    "Post",
+                                    style: GoogleFonts.notoSans(
+                                      fontSize: 16.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: const CircleAvatar(
+                backgroundColor: Color(0xff1f0a45),
+                radius: 25.0,
+                child: Icon(
+                  Icons.send_rounded,
+                  color: Colors.white,
+                ),
+              ),
+            )
           ],
         ),
       ),
       body: Container(
-          margin: EdgeInsets.only(top: 40.0),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          margin: const EdgeInsets.only(top: 40.0, left: 15.0, right: 15.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Comments"),
-              // GestureDetector(
-              //   child: Text("Comments"),
-              //   onTap: () {
-              //     final prediction =
-              //         _classifier.classify("This is a good location.");
-              //     setState(() {
-              //       print("PREDICTION: " + "positive: ${prediction[1]}");
-              //       print("PREDICTION: " + "negative: ${prediction[0]}");
-              //     });
-              //   },
-              // ),
+              Text(
+                "Comments",
+                textAlign: TextAlign.start,
+                style: GoogleFonts.notoSans(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Expanded(
+                // width: MediaQuery.of(context).size.width - 30,
+                // height: MediaQuery.of(context).size.height - 100,
+                // margin: const EdgeInsets.only(top: 15.0),
+                child: MediaQuery.removePadding(
+                  removeTop: true,
+                  context: context,
+                  child: ListView.builder(
+                      itemCount: commentsList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            // color: Colors.green.withOpacity(0.15),
+                            color: commentsList[index].status == 0
+                                ? Color(0xffF5EBEB)
+                                : Color(0xffE5F4F0),
+                          ),
+                          padding: EdgeInsets.all(15.0),
+                          width: MediaQuery.of(context).size.width - 30,
+                          margin: EdgeInsets.only(top: 10.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              CircleAvatar(
+                                radius: 23,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25),
+                                  child: CachedNetworkImage(
+                                    imageUrl: commentsList[index].user_img,
+                                    progressIndicatorBuilder: (context, url,
+                                            downloadProgress) =>
+                                        CircularProgressIndicator(
+                                            value: downloadProgress.progress),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 15.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      commentsList[index].name,
+                                      style: GoogleFonts.notoSans(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    Text(
+                                      commentsList[index].comment,
+                                      style: GoogleFonts.notoSans(
+                                        color: Colors.black.withOpacity(0.7),
+                                      ),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      }),
+                ),
+              ),
             ],
           )),
     );
